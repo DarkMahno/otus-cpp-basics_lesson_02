d815bc8743f17add588cf3cf7d94731487ff1643
Anton Kononchik <Kononchik.A.Geor@sberbank.ru>
2024-01-14 18:47:12 +0300

    first commit

---
+++ todo_move_me_out.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Конончик Антон Георгиевич on 14.01.2024.
+//
+
+#include "todo_move_me_out.h"
+
+#include <iostream>
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
---
+++ todo_move_me_out.h
@@ -0,0 +1,10 @@
+//
+// Created by Конончик Антон Георгиевич on 14.01.2024.
+//
+
+#ifndef OTUS_CPP_BASICS_LESSON_02_TODO_MOVE_ME_OUT_H
+#define OTUS_CPP_BASICS_LESSON_02_TODO_MOVE_ME_OUT_H
+
+#endif //OTUS_CPP_BASICS_LESSON_02_TODO_MOVE_ME_OUT_H
+
+void todo_move_me_out();
